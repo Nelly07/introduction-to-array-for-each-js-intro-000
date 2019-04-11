4e4795f926747e8b0ae0407eb90a2d8d60d33924
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-11 03:59:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-var evens = [0, 2, 4, 6, 8, 10];
-
-evens.forEach(even => {
-  console.log(`${even} is not odd!`);
-});
-
 // Add your doToElementsInArray() function here:
-var evens = [0, 2, 4, 6, 8, 10];
-
-evens.forEach((even, index, array) => {
-  debugger;
-  console.log(`${even} is not odd!`);
-});
-
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 // Add your changeCompletely() function here:
-var evens = [0, 2, 4, 6, 8, 10];
-
-function evenFn(even, index, array) {
-  console.log(`${even} is not odd!`);
-}
-
-for (let i = 0; i < evens.length; i++) {
-  evenFn(evens[i], i, evens);
-}
-
-
-var evens = [0, 2, 4, 6, 8, 10];
-
-function doToEvens(callback) {
-  evens.forEach(callback);
-}
